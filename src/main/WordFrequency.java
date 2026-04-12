b065627a3eb9642c02a91a26bfe221c2be095e41
kanaksng123-arch <kanaksng123.in>
2026-04-12 14:01:54 +0530

    add the code of wordfrequency

--- src/main/WordFrequency.java
+++ src/main/WordFrequency.java
@@ -1,5 +1,23 @@
 package main;
 
 public class WordFrequency {
+    private String word;
+    private int count;
     
+    public WordFrequency(String word , int count){
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord(){
+        return word;
+    }
+    public int getCount(){
+        return count;
+    }
+    @Override
+    public String toString(){
+        return word + " : " + count ; 
+    }
+
 }
